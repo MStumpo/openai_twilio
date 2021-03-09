0762db1e66a75167c2e7b70378e97c1da3904224
Stumpo <stumpo.miguel@gmail.com>
2021-03-09 12:06:36 +0000

    Relocated app.run block to run.py, removed config

--- main.py
+++ main.py
@@ -43,11 +43,4 @@ def sms_reply():
 @app.route("/", methods=['GET'])
 def index():
     return "Ahoy"
-if __name__ == "__main__":
-    app.config.from_mapping(
-        # a default secret that should be overridden by instance config
-        SECRET_KEY="dev",
-        # store the database in the instance folder
-        DATABASE=os.path.join(app.instance_path, "flaskr.sqlite"),
-    )
-    app.run(debug=True)
+
